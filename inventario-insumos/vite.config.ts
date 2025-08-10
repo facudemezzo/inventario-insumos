d76284fad8e0f5634798a8ecfc4532f9baf121fe
facudemezzo <em.cn.demezzo.facundo@gmail.com>
2025-08-10 20:11:59 -0300

    Update vite.config.ts

--- inventario-insumos/vite.config.ts
+++ inventario-insumos/vite.config.ts
@@ -1,5 +1,5 @@
-import { defineConfig } from 'vite';
-import react from '@vitejs/plugin-react';
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
@@ -7,4 +7,6 @@ export default defineConfig({
   optimizeDeps: {
     exclude: ['lucide-react'],
   },
-});
+  base: '/inventario-insumos/', // 👈 nombre EXACTO de tu repo
+})
+
